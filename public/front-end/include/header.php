fb2c8a6927c624f53e713ea501716102d909ac68
s3543536 <1wsx10@gmail.com>
2018-04-11 15:09:41 +1000

    customer info without all the header and footer stuff... its basically nothing at the moment

--- public/front-end/include/header.php
+++ public/front-end/include/header.php
@@ -7,9 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Car Sharing Site" />
 	<meta name="keywords" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
-
-	<!-- TODO thein to insert his surname below -->
-	<meta name="author" content="Anesu Chiodze, Angel English, Jacobde Bruyn, Thien (surname?)" />
+	<meta name="author" content="Anesu Chiodze, Angel English, Jacobde Bruyn, Thien Duc Thi" />
 
   	<!-- Facebook and Twitter integration -->
 	<meta property="og:title" content=""/>
