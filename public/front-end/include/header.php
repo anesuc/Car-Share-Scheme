f95e4dd24b1421f0e66a0f373e242a87bc13367e
Anesu Chiodze <s3542943@student.rmit.edu.au>
2018-04-20 23:59:10 +1000

    Fixed errors in the header file
    Along with changing the link of the home page icon from index.html to the php version, I have also removed unnecessary booking options from the header. The user can view these on the booking page.

--- public/front-end/include/header.php
+++ public/front-end/include/header.php
@@ -54,18 +54,11 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-3 col-xs-2">
-					<div id="fh5co-logo"><a href="index.html">Car Share Scheme.</a></div>
+					<div id="fh5co-logo"><a href="index.php">Car Share Scheme.</a></div>
 				</div>
 				<div class="col-md-6 col-xs-6 text-center menu-1">
 					<ul>
-						<li class="has-dropdown">
-							<a href="product.html">Book a car</a>
-							<ul class="dropdown">
-								<li><a href="single.html">Basic</a></li>
-                                <li><a href="single.html">(Whatever this is called)</a></li>
-                                <li><a href="single.html">Premium</a></li>
-							</ul>
-						</li>
+						<li><a href="book.php">Book a car</a></li>
 						<li><a href="about.html">About</a></li>
 						<li><a href="contact.html">Contact</a></li>
 					</ul>
