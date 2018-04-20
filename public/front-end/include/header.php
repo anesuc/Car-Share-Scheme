7501473def63c9fe35d55607c8aabe4c261f3633
Anesu Chiodze <s3542943@student.rmit.edu.au>
2018-04-20 23:40:13 +1000

    Added footer contents and changed header keywords to somethig useful
    There is now a footer with essentially our names and the "company" social media icons. I only added these as to make it look more "pro" and the footer was kinda empty.

--- public/front-end/include/header.php
+++ public/front-end/include/header.php
@@ -6,7 +6,7 @@
 	<title>Car Share Scheme.</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Car Sharing Site" />
-	<meta name="keywords" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+	<meta name="keywords" content="Car, Share, Scheme, Luxury, Affordable" />
 	<meta name="author" content="Anesu Chiodze, Angel English, Jacobde Bruyn, Thien Duc Thi" />
 
   	<!-- Facebook and Twitter integration -->
