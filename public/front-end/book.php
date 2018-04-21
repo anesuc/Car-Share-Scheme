6493d564edd44e24ac1acee4c34ec41fe483a9f7
Anesu Chiodze <s3542943@student.rmit.edu.au>
2018-04-21 23:25:01 +1000

    Front-end setup of the initial booking page view
    Setting up the booking page initial layout

---
+++ public/front-end/book.php
@@ -0,0 +1,22 @@
+<?php require_once("include/header.php"); ?>
+
+	<div id="book-header">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center booking-info">
+					<h2>Select the type of car you are interested in</h2>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-8 text-center col-md-offset-2">
+                            <a href="#" class="btn btn-primary btn-outline btn-lg">Basic</a>
+                            <a href="#" class="btn btn-primary btn-outline btn-lg">Average</a>
+                            <a href="#" class="btn btn-primary btn-outline btn-lg">Premium</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+<?php require_once("include/footer.php"); ?>
+
