4a3dab414c7e13f15e533f20d90dc01740404972
Anesu Chiodze <s3542943@student.rmit.edu.au>
2018-04-21 23:31:30 +1000

    Fixed the wrong year in the footer

--- public/front-end/include/footer.php
+++ public/front-end/include/footer.php
@@ -14,7 +14,7 @@
 			<div class="row copyright">
 				<div class="col-md-12 text-center">
 					<div class="footer-info">
-						<small class="block">&copy; 2017 Car Share Scheme.</small>
+						<small class="block">&copy; 2018 Car Share Scheme.</small>
                         <!-- _blank to open in a new tab so that the user doesn't lose the page -->
 						<small class="block">Developed by <a href="https://twitter.com/AnesuChiodze" target="_blank">Anesu Chiodze</a>, <a href="#">Angel English</a>, <a href="#">Jacob De Bruyn</a>, <a href="#">Thien Thi</a></small>
 					</div>
