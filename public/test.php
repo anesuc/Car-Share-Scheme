104f95d84801715bb0a744c35d26af70fd4adb41
s3543536 <1wsx10@gmail.com>
2018-04-30 12:55:41 +1000

    changed it to new way of adding an api funciton

--- public/test.php
+++ public/test.php
@@ -5,25 +5,16 @@ ini_set("display_errors", 1);
 ini_set("display_startup_errors", 1);
 error_reporting(E_ALL);
 
-print("test\n");
-try {
-	require App\Http\Controllers\BookingController as Cont;
-} catch(Exception $E) {
-	print($E->getMessage());
-}
 
 
 $format = "Y-m-d H:i:s";
 
-$start = date($format);
-$end = date($format);
+$startTime = date($format);
+$endTime = date($format);
+$startLoc = 3;
+$endLoc = 1;
+$id = 2;
 
-print("test\n");
-try {
-	Cont::add_booking(2, 3, $start, $end, 2);
-} catch(Exception $E) {
-	print($E->getMessage());
-}
-print("success... ?");
+header("Location: https://cars.dev/add_booking/start_loc=$startLoc&end_loc=$endLoc&start_time=$startTime&end_time=$endTime&car_id=$id");
 
 ?>
