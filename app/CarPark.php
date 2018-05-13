9eb190c1934e43d4db371a04ee290b3c60b6ac0c
rmit-s3433889-jacob-debruyn <s3433889@student.rmit.edu.au>
2018-05-13 22:27:53 +1000

    json output of carparks

--- app/CarPark.php
+++ app/CarPark.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class CarPark extends Model
+class Carpark extends Model
 {
     //
 }
--- app/Http/Controllers/CarParkController.php
+++ app/Http/Controllers/CarParkController.php
@@ -3,40 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\CarPark;
+use App\Carpark;
 use App\Booking;
 use App\Car;
 use DateTime;
 class CarParkController extends Controller
 {
 	public function find_within($lat, $lng, $lat2, $lng2) {
-		$result = CarPark::whereRaw('latitude > ? and longitude > ? and latitude < ? and longitude < ?', [$lat, $lng, $lat2, $lng2])->get();
+		$result = Carpark::whereRaw('latitude > ? and longitude > ? and latitude < ? and longitude < ?', [$lat, $lng, $lat2, $lng2])->get();
 
 		echo var_dump($result);
 		return $result;
 	}
 
 	//radius is in km
-	public function find_within($lat, $lng, $radius) {
+	public function find_within_radius($lat, $lng, $radius) {
 		$radius /= 111;//convert km to lat/lng (degrees around earth)
 		$radiusSqr = $radius * $radius;
-		$result = CarPark::whereRaw('SQUARE(latitude - ?) + SQUARE(longitude - ?) < ?', [$lat, $lng, $radiusSqr])->get();
+		$result = Carpark::whereRaw('SQUARE(latitude - ?) + SQUARE(longitude - ?) < ?', [$lat, $lng, $radiusSqr])->get();
 
 		echo var_dump($result);
 		return $result;
 	}
 
 	public function find_exact($lat, $lng) {
-		$result = CarPark::whereRaw('latitude = ? and longitude = ?', [$lat, $lng])->get();
+		$result = Carpark::whereRaw('latitude = ? and longitude = ?', [$lat, $lng])->get();
 
 		echo var_dump($result);
 		return $result;
 	}
 
 	public function find_all() {
-		$all = CarPark->get();
-
-		echo var_dump($all)
-		return $all;
+		$all = Carpark::all();
+		$json = json_encode($all);
+		echo $json;
 	}
 }
